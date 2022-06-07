12d21b8e8e5a8ff4baeced3dfb25b6ff59d4a337
joyjk <krmkrjoy@gmail.com>
2022-06-07 12:25:37 +0600

    calculator added

---
+++ main.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require('electron')
+function createWindow(){
+    const win = new BrowserWindow({
+        width:425,
+        height:800,
+        webPreferences:{
+            nodeIntegration:true
+        }
+    })
+    win.loadFile('index.html')
+
+}
+app.whenReady().then(createWindow);
